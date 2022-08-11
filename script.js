257066dad8c96337e3f6c9dd7aa361a8a6469d30
HJ-C <swch56@naver.com>
2022-08-11 10:20:26 +0900

    #2

---
+++ script.js
@@ -0,0 +1,12 @@
+const body=document.querySelector('body')
+const sidebar=document.getElementById('nav')
+const toggle=body.querySelector('.toggle')
+const searchBtn=body.querySelector('.search-box')
+
+
+toggle.addEventListener('click',()=>{
+    sidebar.classList.toggle('close')
+})
+searchBtn.addEventListener('click',()=>{
+    sidebar.classList.remove('close')
+})
